ca00c2eacff5e920f4f68f33e4eb19fa5abcd87e
abdullrahmanwalidshawky <abdulrahmanwld@gmail.com>
2025-11-12 15:29:07 +0200

    Update Visitor.h

--- Visitor.h
+++ Visitor.h
@@ -1,15 +1,20 @@
-#ifndef VISITOR_H
-#define VISITOR_H
 
+
+#ifndef VISTOR_H_
+#define VISTOR_H_
 #include <string>
 using namespace std;
 
-//define visitor class here
+class Vistor {
+private:
+	string visitorName;
+	int ticketsBought;
+public:
+	Vistor();
+	Vistor(string visitorName, int ticketsBought);
+	virtual ~Vistor();
+	virtual void displayAnimals();
+};
 
+#endif /* VISTOR_H_ */
 
-
-
-
-
-
-#endif
