60d5767da5ffc683b37fe52529bfcbcdf3653d12
abdullrahmanwalidshawky <abdulrahmanwld@gmail.com>
2025-11-12 15:30:01 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,16 +1,21 @@
-#include "Animal.h"
+
+#include "Vistor.h"
+#include "Enclosure.h"
 #include "Bird.h"
 #include "Mammal.h"
+#include "Animal.h"
 #include "Reptile.h"
-#include "Enclosure.h"
-#include "Visitor.h"
+#include <string>
 #include <iostream>
 using namespace std;
-
-
-// ============== MAIN FUNCTION ==============
-int main() {
-    
-    
-    return 0;
+int main(){
+	Animal aa[3];
+	Enclosure enc (aa, 10, 5);
+   Mammal m1("ji",3, false, "red");
+   Bird b1("ant", 1, false, 1.33);
+   Reptile r1("yuu", 2, true, false);
+   Vistor v1("ahmed", 2);
+   enc.displayAnimals();
+   v1.displayAnimals();
 }
+
